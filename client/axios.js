d7d4109bf73dc8f0defc2f322a8b954ed0669ec0
Nabil-4 <126590136+Nabil-4@users.noreply.github.com>
2025-03-28 16:52:15 +0100

    Kralle

--- client/axios.js
+++ client/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = 'Adress ip and port of your backend server';
+export const baseURL = 'ip address and port of your backend server';
 
 export const Request = axios.create({
     baseURL: baseURL,
